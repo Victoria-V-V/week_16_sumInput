33da4789792d66051820b3eed7768fc558a7ba75
Victoria-V-V <victoriavolcheck@yandex.ru>
2022-02-08 20:08:11 +0700

    Added html & js

---
+++ index.js
@@ -0,0 +1,15 @@
+function sumInput() {
+    let numbers = [];
+    let sum = 0;
+
+    for (;;) {
+        let number = +prompt("Введите значение");
+        if (!number) break;
+        numbers.push(number);
+        sum += number;
+    }
+    let numbers2 = numbers.slice();
+    numbers2.sort((a, b) => a - b);
+
+    alert('Исходный массив ' + numbers + '\n' + 'Отсортированный массив ' + numbers2 + '\n' + 'Сумма: ' + sum);
+}
